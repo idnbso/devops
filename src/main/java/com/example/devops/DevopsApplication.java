eeed004d8fc82d8cee183a4aee0704f967880abe
Idan Busso <idanbusso@gmail.com>
2020-06-30 21:09:32 +0300

    Added logging to the welcome controller

--- src/main/java/com/example/devops/DevopsApplication.java
+++ src/main/java/com/example/devops/DevopsApplication.java
@@ -2,12 +2,19 @@ package com.example.devops;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.StandardEnvironment;
 
 @SpringBootApplication
 public class DevopsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DevopsApplication.class, args);
-	}
+		ConfigurableEnvironment environment = new StandardEnvironment();
+		environment.setActiveProfiles("dev");
 
+		SpringApplication sa = new SpringApplication(DevopsApplication.class);
+		sa.setEnvironment(environment);
+		sa.setAdditionalProfiles("qa", "prod");
+		sa.run(args);
+	}
 }
