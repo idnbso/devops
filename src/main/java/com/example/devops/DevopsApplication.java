755230d7f0e78c6b76e25f8f238939760018e758
Idan Busso <idanbusso@gmail.com>
2020-07-01 22:02:50 +0300

    Refactor project environment variables configuration

--- src/main/java/com/example/devops/DevopsApplication.java
+++ src/main/java/com/example/devops/DevopsApplication.java
@@ -2,19 +2,11 @@ package com.example.devops;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.env.ConfigurableEnvironment;
-import org.springframework.core.env.StandardEnvironment;
 
 @SpringBootApplication
 public class DevopsApplication {
 
 	public static void main(String[] args) {
-		ConfigurableEnvironment environment = new StandardEnvironment();
-		environment.setActiveProfiles("dev");
-
-		SpringApplication sa = new SpringApplication(DevopsApplication.class);
-		sa.setEnvironment(environment);
-		sa.setAdditionalProfiles("qa", "prod");
-		sa.run(args);
+		SpringApplication.run(DevopsApplication.class, args);
 	}
 }
