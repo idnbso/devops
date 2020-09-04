631a665f9da48c442e00159329ec86ca4a768ddf
Idan Busso <idanbusso@gmail.com>
2020-09-04 11:44:38 +0300

    Refactor utils.groovy functions structure

--- utils.groovy
+++ utils.groovy
@@ -13,36 +13,38 @@ def getACMReportsIncrementedVersion() {
     final numberSeparatorToken = '.'
     final versionFormat = "ENVIRONMENT${numberSeparatorToken}MAJOR${numberSeparatorToken}MINOR${numberSeparatorToken}PATCH"
 
-    final incrementedVersion = getIncrementedVersion(versionFile.version, versionFormat, 
-        maximumTotalVersionNumbers, minimumTotalVersionNumbers, partsSeparatorToken, numberSeparatorToken)
+    // Build incremented version
+    final versionParts = versionFile.version.tokenize(partsSeparatorToken)
+    final versionPrefix = versionParts.subList(0, versionParts.size() - 1).join(partsSeparatorToken)
+    final versionBuildNumber = versionParts.last()
+
+    final incrementedVersionNumber = getIncrementedVersionNumber(versionBuildNumber, versionFormat, numberSeparatorToken
+                                        maximumTotalVersionNumbers, minimumTotalVersionNumbers)
+    final incrementedVersion = "${versionPrefix}_${incrementedVersionNumber}"            
     println("Incremented Version: ${incrementedVersion}")
 
     return incrementedVersion
 }
 
-def getIncrementedVersion(version, versionFormat, maximumTotalVersionNumbers, minimumTotalVersionNumbers, 
-                          partsSeparatorToken, numberSeparatorToken) {
-    final versionParts = version.tokenize(partsSeparatorToken)
-    final versionPrefix = versionParts.subList(0, versionParts.size() - 1).join(partsSeparatorToken)
-
-    final versionBuildNumber = versionParts.last()
+def getIncrementedVersionNumber(versionBuildNumber, versionFormat, numberSeparatorToken
+                                maximumTotalVersionNumbers, minimumTotalVersionNumbers) {
     final versionNumbers = versionBuildNumber.tokenize(numberSeparatorToken)
-    def incrementedVersionNumbers = ""
+    String incrementedVersionNumber
 
     if (versionNumbers.size() < maximumTotalVersionNumbers && versionNumbers.size() >= minimumTotalVersionNumbers) {
-        incrementedVersionNumbers = "${versionBuildNumber}${numberSeparatorToken}1"
+        incrementedVersionNumber = "${versionBuildNumber}${numberSeparatorToken}1"
     }
     else if (versionNumbers.size() == maximumTotalVersionNumbers) {
         final majorVersionNumbers = versionNumbers.subList(0, versionNumbers.size() - 1).join(numberSeparatorToken)
         final minorVersionNumber = Integer.parseInt(versionNumbers.last())
-        incrementedVersionNumbers = "${majorVersionNumbers}${numberSeparatorToken}${minorVersionNumber + 1}"
+        incrementedVersionNumber = "${majorVersionNumbers}${numberSeparatorToken}${minorVersionNumber + 1}"
     }
     else {
         throw new IllegalArgumentException(
             "Illegal version number input. Must be compatible with following format: ${versionFormat}")
     }
 
-    return "${versionPrefix}_${incrementedVersionNumbers}"
+    return incrementedVersionNumber
 }
 
 return this
